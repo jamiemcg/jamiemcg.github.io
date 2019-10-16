3329715ecbc30ca9762df2e08e73d524a395ceeb
Jamie McGowan <jamiemcgowan.dev@gmail.com>
2019-10-16 21:33:55 +0100

    CV website uploaded

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $("#link-jamie").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, 500);
+    });
+    
+    $("#link-about").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, 500);
+    });
+    
+    $("#link-education").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $("#education").offset().top - 64
+        }, 500);
+    });
+
+    $("#link-publications").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $("#publications").offset().top - 64
+        }, 500);
+    });
+
+    $("#link-awards").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $("#awards").offset().top - 64
+        }, 500);
+    });
+
+    $("#link-presentations").click(function(e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: $("#presentations").offset().top - 64
+        }, 500);
+    });
+
+    // Hide nav panel if shown after clicking link
+    $('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+    });
+})
